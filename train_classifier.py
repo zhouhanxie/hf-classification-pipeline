f876b4f4d7e53cf9a3a88e9ac9e99f1384132ebd
zhouhanxie <zhouhanx@uci.edu>
2023-06-12 10:10:40 -0700

    update

--- train_classifier.py
+++ train_classifier.py
@@ -16,64 +16,37 @@ import joblib
 import os
 
 
-def to_bitfit(model, verbose=False):
-    """
-    turn off anything except bias and classification head 
-    in a transformer model
-    """
-    if verbose:
-        print('most parameters will be turned off.')
-    for name, param in model.named_parameters():
-        param.requires_grad = False
-    for name, param in model.lm_head.named_parameters():
-        param.requires_grad = True
-        
-    return model
-
 def main(args):
-    data = pd.read_csv(args.data_path)
+    from datasets import load_dataset
 
-    data['input'] = data['sentence']
+    dataset = load_dataset("sst2")
 
-    le = preprocessing.LabelEncoder()
-    data['labels'] = le.fit_transform(data['type'])
-
-    joblib.dump(le, os.path.join(args.output_dir, 'label_encoder.joblib'))
-
-    train_df, test_df = train_test_split(data, test_size=0.1, random_state=0)
-    # train_df = pd.DataFrame({'source':list(train['input'].astype(str)), 'target':list(train['output'].astype(str))})
-    train_dataset = Dataset.from_pandas(train_df)
-    # test_df = pd.DataFrame({'source':list(test['input']), 'target':list(test['output'])})
-    test_dataset = Dataset.from_pandas(test_df)
+    train_dataset = dataset['train']
+    val_dataset = dataset['validation']
 
     tokenizer = AutoTokenizer.from_pretrained(args.base_model_name_or_path)
 
     def preprocess_function(examples):
-        inputs = examples['input']
+        inputs = examples['sentence']
         model_inputs = tokenizer(text = inputs, max_length=148, padding='max_length', truncation=True)
         
         return model_inputs
 
     train_dataset = train_dataset.map(preprocess_function)
-    test_dataset = test_dataset.map(preprocess_function)
+    val_dataset = val_dataset.map(preprocess_function)
 
-    label2id = dict(zip(le.classes_, le.transform(le.classes_)))
-    id2label = {v: k for k, v in label2id.items()}
-    print(label2id)
     model = AutoModelForSequenceClassification.from_pretrained(
         args.base_model_name_or_path,
-        num_labels=len(label2id)
-        )
+        num_labels= len(set(dataset['train']['label']))
+    )
 
-    # if you really can't fit your model in try bitfit, which only adjusted bias terms
-    # model = to_bitfit(model, verbose=True)
 
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     trainer = CustomTrainer(
         model=model,
         train_dataset=train_dataset, 
-        eval_dataset=test_dataset,
+        eval_dataset=val_dataset,
         tokenizer=tokenizer,
         data_collator=data_collator,
         args=CustomTrainingArguments(
@@ -105,8 +78,8 @@ if __name__ == '__main__':
     
     from easydict import EasyDict as edict
     args = dict(
-        data_path='./data/political_stance/stance_detection.csv', 
-        base_model_name_or_path="launch/POLITICS",
+        data_path='', 
+        base_model_name_or_path="bert-base-uncased",
         output_dir='./checkpoints',
         num_train_epochs=10,
         per_device_train_batch_size=1,
